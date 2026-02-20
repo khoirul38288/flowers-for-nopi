acb8c1f3d1f6d15fd3dc29fe950e787b4d8d79f4
khoirul38288 <khoirul38288@gmail.com>
2026-02-20 23:27:10 +0700

    main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('NOPI ANJING 💐💐💐').split('')
+    const titles = ('This Flower Is For You').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
